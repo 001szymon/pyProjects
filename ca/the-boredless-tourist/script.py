a6b8aeef1e07c8a2ab164f0776894108aaa2cf26
Szymon Piątkowski <29815075+SzymonPiatkowski@users.noreply.github.com>
2020-04-06 14:37:13 +0200

    Update script.py

--- ca/the-boredless-tourist/script.py
+++ ca/the-boredless-tourist/script.py
@@ -59,21 +59,34 @@ def find_attractions(dest, interests):
   possible_attraction = [x for x in attractions_in_city]
   
   for x in possible_attraction:
-    print(x, "|| possible_attraction")
+    #print(x, "|| possible_attraction")
     attraction_tags = x[1]
-    print(attraction_tags, "                || attraction_tags")
+    #print(attraction_tags, "                || attraction_tags")
     
     for i in interests:
-      print(i, "                             || interests")
+      #print(i, "                             || interests")
       if i in attraction_tags:
-        print(i, "                         || if")
-        attractions_with_interest.append(x) 
+        #print(i, "                         || if")
+        attractions_with_interest.append(x[0]) 
   return attractions_with_interest
 
 la_arts = find_attractions("Los Angeles, USA", ["art"])
-print(la_arts)
+#print(la_arts)
   
-  
-  
-  
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+  interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler[1] + ": "
+  for i in traveler_attractions:
+    interests_string += i
+    if i != traveler_attractions[-1]:
+      interests_string += ", "
+    else:
+      interests_string += "."
+  return interests_string
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
+
   
